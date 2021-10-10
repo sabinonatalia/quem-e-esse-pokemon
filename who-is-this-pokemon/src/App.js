ede24613ca87ad2512fa57ee4843b165230278f4
Natália <na.ssilva@outlook.com>
2021-10-09 22:42:38 -0300

    resolvido conflito de merge

--- who-is-this-pokemon/src/App.js
+++ who-is-this-pokemon/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function App() {
   // return (
   //   <div className="App">
@@ -18,30 +17,6 @@ function App() {
   //     </header>
   //   </div>
   // );
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 98f4b41e3f552de8e237f548699732057b8194fa
-}
 
 export default App;
+}
