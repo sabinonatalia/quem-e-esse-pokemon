a42eb29a191b3aa7f64008344395724081a84a17
Natália <na.ssilva@outlook.com>
2021-10-09 22:15:23 -0300

    configuração do projeto e finalização da tela home

---
+++ who-is-this-pokemon/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Home } from './pages/Home';
+
+// import App from './App';
+import "./assets/styles/global.scss";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
